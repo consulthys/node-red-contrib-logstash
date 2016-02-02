390c613afc53f2fe2a1f6d68dbc34422d5802252
val <valentin.crettaz@consulthys.com>
2016-02-02 14:39:22 +0100

    Added support for all logstash conditionals

--- nr2ls.js
+++ nr2ls.js
@@ -23,7 +23,20 @@ if (!flowId) {
 getFlow(flowId, generate);
 
 var ops = {
-    eq: "=="
+    'eq': function(p, v){return util.format('[%s] == "%s"', p, v);},
+    'neq': function(p, v){return util.format('[%s] != "%s"', p, v);},
+    'lt': function(p, v){return util.format('[%s] < "%s"', p, v);},
+    'lte': function(p, v){return util.format('[%s] <= "%s"', p, v);},
+    'gt': function(p, v){return util.format('[%s] > "%s"', p, v);},
+    'gte': function(p, v){return util.format('[%s] >= "%s"', p, v);},
+    'btwn': function(p, v, v2){return util.format('[%s] >= %s and [%s] <= %s', p, v, p, v2);},
+    'cont': function(p, v){return util.format('"%s" in [%s]', v, p);},
+    'regex': function(p, v){return util.format('[%s] =~ /%s/', p, v);},
+    'true': function(p, v){return util.format('[%s]', p);},
+    'false': function(p, v){return util.format('![%s]', p);},
+    'null': function(p, v){return util.format('![%s]', p);},
+    'nnull': function(p, v){return util.format('[%s]', p);},
+    'else': function(p, v){return '1 == 1';}
 };
 
 function getFlow(flowId, callback) {
@@ -35,13 +48,13 @@ function getFlow(flowId, callback) {
     };
     var flowReq = http.request(reqOptions, function(flowRes) {
         var data = [];
+        flowRes.on('close', function (error) {});
         flowRes.on('data', function (chunk) {
             data.push(chunk);
         });
-        flowRes.on('close', function (error) {
-        });
         flowRes.on('end', function() {
             var results = data.length > 0 ? JSON.parse(data.join("")) : {};
+            //console.log(JSON.stringify(results, null, 3));
             callback(null, results);
         });
 
@@ -68,7 +81,7 @@ function generate(err, flow) {
 
         // handle logstash nodes
         if (type.length === 3 && type[0] === "ls") {
-            var component = {"_name": type[2]};
+            var component = {"_name": type[2], "_rule": []};
             for (var prop in node) {
                 if (/^ls_/.test(prop) && node[prop]) {
                     component[prop.substring(3)] = node[prop];
@@ -91,12 +104,33 @@ function generate(err, flow) {
 
             rules.forEach(function(rule, index) {
                 var wire = node.wires[index][0];
-                if (wire && components.flt.elements[wire]) {
-                    components.flt.elements[wire]._rule = util.format("[%s] %s \"%s\"", node.property, ops[rule.t], rule.v);
+                var comp = components.flt.elements[wire] || components.out.elements[wire];
+                if (wire && comp) {
+                    var args = [node.property, rule.v];
+                    if (rule.v2) args.push(rule.v2);
+
+                    if (ops[rule.t]) {
+                        comp._rule.push(ops[rule.t].apply(null, args));
+                    }
                 }
             });
         }
     });
+    //console.log(JSON.stringify(components, null, 2));
+
+    function __generateElement(lines, indent, element) {
+        lines.push(util.format(indent + "  %s {", element._name));
+        for (var prop in element) {
+            if (!/^_/.test(prop) && element[prop]) {
+                if (/^\[/.test(element[prop]) || /^\{/.test(element[prop])) {
+                    lines.push(util.format("      %s => %s", prop, element[prop]));
+                } else {
+                    lines.push(util.format("      %s => \"%s\"", prop, element[prop]));
+                }
+            }
+        }
+        lines.push(util.format(indent + "  }"));
+    }
 
     var lines = [];
     ["in", "flt", "out"].forEach(function(sectionId) {
@@ -105,24 +139,19 @@ function generate(err, flow) {
         for (var elemId in section.elements) {
             var element = section.elements[elemId];
             var indent = "";
-            if (element._rule) {
-                lines.push(util.format("  if %s {", element._rule));
-                indent = "  ";
+            if (element._rule.length > 0) {
+                element._rule.forEach(function(_rule) {
+                    lines.push(util.format("  if %s {", _rule));
+                    indent = "  ";
+                    lines.push(util.format("    # " + element._comment));
+                    __generateElement(lines, indent, element);
+                    lines.push("  }");
+                });
             }
-            if (element._comment) {
+            else {
                 lines.push(util.format("  # " + element._comment));
+                __generateElement(lines, indent, element);
             }
-            lines.push(util.format(indent + "  %s {", element._name));
-            for (var prop in element) {
-                if (!/^_/.test(prop) && element[prop]) {
-                    if (/^\[/.test(element[prop])) {
-                        lines.push(util.format("      %s => %s", prop, element[prop]));
-                    } else {
-                        lines.push(util.format("      %s => \"%s\"", prop, element[prop]));
-                    }
-                }
-            }
-            lines.push(util.format(indent + "  }"));
         }
         lines.push(util.format("}"));
     });
